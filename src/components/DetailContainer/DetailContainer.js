e7e70d36ed35fe3a0e1222c5ca8efa5d1a5deb87
Aaron Lei <aaron.l@live.ca>
2021-04-25 18:17:53 -0400

    created sizing buttons in detailed component"

--- src/components/DetailContainer/DetailContainer.js
+++ src/components/DetailContainer/DetailContainer.js
@@ -1,9 +1,71 @@
 import React from "react";
+import "../../App.css";
+import Button from "../Button/Button";
+import "./DetailContainer.css";
 
 function DetailContainer() {
+  const title = "Red Flyknit Trainers";
+  const price = "$190.00";
+
+  const handleToggle = (e) => {
+    e.target.style.background = "#1d1d1d";
+    e.target.style.color = "#fff";
+  };
   return (
-    <div>
-      <h1>Detail Container</h1>
+    <div className="detail-container">
+      <div className="text-details">
+        <h1>{title}</h1>
+        <section>{price}</section>
+      </div>
+      <p>Size</p>
+      <div className="button-container">
+        <Button
+          id="small"
+          className="btn"
+          buttonStyle="btn--primary"
+          buttonToggle="btn_toggle"
+          buttonSize="btn--small"
+          onClick={(e) => {
+            handleToggle(e);
+          }}
+        >
+          Small
+        </Button>
+        <Button
+          id="medium"
+          className="btn"
+          buttonStyle="btn--primary"
+          buttonSize="btn--small"
+          onClick={(e) => {
+            handleToggle(e);
+          }}
+        >
+          Medium
+        </Button>
+        <Button
+          id="large"
+          className="btn"
+          buttonStyle="btn--primary"
+          buttonSize="btn--small"
+          onClick={(e) => {
+            handleToggle(e);
+          }}
+        >
+          Large
+        </Button>
+        <Button
+          className="btn"
+          id="extra-large"
+          buttonStyle="btn--primary"
+          buttonSize="btn--small"
+          onClick={(e) => {
+            handleToggle(e);
+          }}
+        >
+          Extra Large
+        </Button>
+      </div>
+      <hr />
     </div>
   );
 }
--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import DetailContainer from "../DetailContainer/DetailContainer";
 import ImageContainer from "../ImageContainer/ImageContainer";
+import "./MainContainer.css";
 
 function MainContainer() {
   return (
     <div>
       <h1> Main Section </h1>
-      <ImageContainer />
+      <div className="image-container">
+        <ImageContainer />
+      </div>
       <div className="details-container">
         <DetailContainer />
       </div>
