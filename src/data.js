b38e7d507f73b8d9149d66e3bf2757a4b5b0d78e
Aaron Lei <aaron.l@live.ca>
2021-04-25 17:18:14 -0400

    added data filee

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const imageData = [
+  { src: "images/red.png", alt: "Red Flyknit Trainers", id: "red" },
+  {
+    src: "images/green.png",
+    alt: "Green Flyknit Trainers",
+    id: "green",
+  },
+  { src: "images/blue.png", alt: "Blue Flyknit Trainers", id: "blue" },
+];
