3937ecb77af820d809b8f9d78d7009299deeafef
Aaron Lei <aaron.l@live.ca>
2021-04-25 18:42:45 -0400

    Updates

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export const imageData = [
-  { src: "images/red.png", alt: "Red Flyknit Trainers", id: "red" },
-  {
-    src: "images/green.png",
-    alt: "Green Flyknit Trainers",
-    id: "green",
-  },
-  { src: "images/blue.png", alt: "Blue Flyknit Trainers", id: "blue" },
-];
